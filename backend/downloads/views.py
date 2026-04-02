73b88626ddd1aeed86bd638780769d7a3b31d5e4
hommula <bruce2588@gmail.com>
2026-04-01 19:41:58 -0700

    fix coroutine

--- backend/downloads/views.py
+++ backend/downloads/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from subprocess import call
+from asgiref.sync import async_to_sync
 from .download_handler import debrid_download
 from .directory_hanlder import sub_directory_list
 
 @api_view(["POST"])
-async def add_download_queue(req):
+def add_download_queue(req):
     # req.body format:
     # url: download link
     # storedLocation: directory to store
@@ -14,15 +15,15 @@ async def add_download_queue(req):
     # linkProvider: debrid or google or direct
     link_provider = req.data.get("linkProvider")
     if link_provider == "debridDownload":
-        download_result = await debrid_download(req)
-   
+        download_result = async_to_sync(debrid_download)(req)
+
     return Response(
         download_result["downloads"],
         status=download_result["status_code"])
 
 @api_view(["GET"])
-async def get_directory_structure(req):
-    sub_dir_result = await sub_directory_list(req)
+def get_directory_structure(req):
+    sub_dir_result = async_to_sync(sub_directory_list)(req)
 
     return Response(
         sub_dir_result,
